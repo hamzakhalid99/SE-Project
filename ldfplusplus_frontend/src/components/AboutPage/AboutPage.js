10cdc15d70110e34f426c8eaba16f7283992dd55
ZafirAnsari <zafiransari12@gmail.com>
2022-03-27 23:10:30 +0500

    adding about us page

---
+++ ldfplusplus_frontend/src/components/AboutPage/AboutPage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './AboutPage.css';
+
+const AboutPage = () => {
+	return (
+		
+        <main class="landing">
+			<h1 id="social-heading">About Us</h1>
+			
+			<h3 id="happening-heading">The LUMS student body remains
+			 deprived of a single platform which can streamline some very important tasks.</h3>
+			 <h3 id="happening-heading">
+			  Currently, a discussion portal namely LUMS Discussion Forum (LDF) exists where students
+			   can post about their questions and concerns.</h3> 
+			   <h3 id="happening-heading">However, due to the large number of members in LDF, 
+			   it has become more of a formal place where only the most important issues are catered to.</h3>
+			   <h3 id="happening-heading">
+			   For instance, a student who posts about wanting someone to bring food for them from 
+			   some restaurant without having to get it themselves might not get enough recognition as
+			    someone who posts about a major ongoing issue related to campus accommodation.</h3>
+				<h3 id="happening-heading">
+				 However, if the post of the student who wanted food was in a portal which was specifically for
+				  that type of request, it may get more serious and quick recognition. On average, someone who is capable
+				   of delivering the food will see the post quicker than they will on LDF.</h3> 
+				   <h3 id="happening-heading">But this is just one example 
+				   of how LDF++ will benefit the LUMS student body over LDF. The instructor and course reviews
+				    will be present in extremely organized sections and subsections where students can find all reviews of a certain course.
+					 To plan get-togethers for sitting around on campus, or for a jog around the cricket ground, students 
+					 can post on LDF++ in the separate get-together section, where other students can find that particular
+					  post and join in. Similarly, students can post about donation opportunities, enrollment swap requests, 
+					  events, and belongings they want to sell in their own separate portals. A major benefit of using LDF++
+					   over LDF or any other group on social media is that students will not have to share their social media
+					    profiles with the whole community.</h3>
+			
+		</main>
+	)
+}
+
+export default AboutPage;
