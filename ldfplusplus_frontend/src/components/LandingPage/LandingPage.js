b15a66696fdd815fab2cde5b8f57e28af693755d
raffayatiq <raffayatiq2341@gmail.com>
2022-03-27 14:31:21 +0500

    Adding LandingPage component.

---
+++ ldfplusplus_frontend/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+	return (
+		<main class="landing">
+			<h1 id="social-heading">Social Experience</h1>
+			<h1 id="social-heading">Redefined</h1>
+			<h3 id="happening-heading">Find everything and anything happening at LUMS!</h3>
+			<a id="join" class="green-button">Join Now!</a>
+		</main>
+	)
+}
+
+export default LandingPage;
