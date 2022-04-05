396d22bfc751416ab1c886a3538e48c693b89343
raffayatiq <raffayatiq2341@gmail.com>
2022-04-05 23:55:02 +0500

    Minor changes to App.js.

--- ldfplusplus_frontend/src/App.js
+++ ldfplusplus_frontend/src/App.js
@@ -19,8 +19,6 @@ class App extends Component {
   onRouteChange = (route) => {
 		if (route === 'signout') {
 			this.setState({initialState})
-		} else if (route === 'home') {
-			this.setState({isSignedIn: true})
 		}
 		
     this.setState({route: route })
