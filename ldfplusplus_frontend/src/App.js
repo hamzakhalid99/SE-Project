4c822fbedfd9c22229c483bcaa50b475833a9fea
raffayatiq <raffayatiq2341@gmail.com>
2022-04-05 23:53:14 +0500

    Added routing functionality.

--- ldfplusplus_frontend/src/App.js
+++ ldfplusplus_frontend/src/App.js
@@ -5,15 +5,45 @@ import Navigation from './components/Navigation/Navigation.js';
 import LandingPage from './components/LandingPage/LandingPage.js';
 import AboutPage from './components/AboutPage/AboutPage.js';
 
+const initialState = {
+	route: 'landing',
+	isSignedin: false
+}
+
 class App extends Component {
+	constructor() {
+		super();
+		this.state = initialState;
+	}
+
+  onRouteChange = (route) => {
+		if (route === 'signout') {
+			this.setState({initialState})
+		} else if (route === 'home') {
+			this.setState({isSignedIn: true})
+		}
+		
+    this.setState({route: route })
+	}
+
   render() {
-    return (
-      <div className="App">
-        <AboutPage />
-        {/* <Navigation />
-        <LandingPage /> */}
-      </div>
-    );
+  	const { route, isSignedIn } = this.state;
+
+    if (route === 'landing') {
+      return (
+        <div className="App">
+          <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
+          <LandingPage />
+        </div>
+      )
+    } else if (route === 'about') {
+      return (
+        <div className="App">
+          <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
+          <AboutPage />
+        </div>
+      )
+    }
   }
 }
 
--- ldfplusplus_frontend/src/components/Navigation/Navigation.js
+++ ldfplusplus_frontend/src/components/Navigation/Navigation.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './Navigation.css';
 
-const Navigation = () => {
+const Navigation = ({ onRouteChange, isSignedIn }) => {
 	return (
 		<header class="header">
-			<a class="header-logo" href="#">
+			<a onClick={() => onRouteChange(isSignedIn ? 'homepage' : 'landing')} class="header-logo" href="#">
 			<h1 class="header-text">
 				LDF ++
 			</h1>
 			</a>
 			<ul class="header-list aboutcontact">
-				<li><a href="#">About</a></li>
-				<li><a href="#">Contact</a></li>
+				<li><a onClick={() => onRouteChange('about')}>About</a></li>
+				<li><a onClick={() => onRouteChange('contact')}>Contact</a></li>
 			</ul>
 			<nav class="header-nav">
 				<ul class="header-list">
