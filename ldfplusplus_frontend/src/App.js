2a16f6267a97f9fbb609de6b022b113f216b247e
raffayatiq <raffayatiq2341@gmail.com>
2022-04-06 02:23:52 +0500

    Minor fixes and adjustments.

--- ldfplusplus_frontend/src/App.js
+++ ldfplusplus_frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
 import LandingPage from './components/LandingPage/LandingPage.js';
 import AboutPage from './components/AboutPage/AboutPage.js';
+import SignUp from './components/SignUp/SignUp.js';
 
 const initialState = {
 	route: 'landing',
@@ -39,6 +40,13 @@ class App extends Component {
         <div className="App">
           <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
           <AboutPage />
+        </div>
+      )
+    } else if (route === 'signup') {
+      return (
+        <div className="App">
+          <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
+          <SignUp onRouteChange={this.onRouteChange} />
         </div>
       )
     }
