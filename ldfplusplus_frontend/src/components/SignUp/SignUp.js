88b45e3483f15c6dd85a976319d6677e22a5cca6
raffayatiq <raffayatiq2341@gmail.com>
2022-04-06 00:37:00 +0500

    Added Full Name input and integrated with backend on port 3000.

--- ldfplusplus_frontend/src/components/SignUp/SignUp.js
+++ ldfplusplus_frontend/src/components/SignUp/SignUp.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import './SignUp.css';
 
-const SignUp = () => {
-    return ("SignUp")
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            fullname: '',
+            email: '',
+            password: '',
+            confirmpassword: '',
+        }
+    }
+
+    onFullNameChange = (event) => {
+        this.setState({fullname: event.target.value})
+        console.log(this.state.fullname)
+    }
+
+    onSubmitSignUp = () => {
+		fetch('http://localhost:3000/signup', {
+			method: 'post',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(this.state)
+		})
+		.then(response => response.json())
+		.then(user => {
+			if (user.id) {
+				this.props.loadUser(user);
+				this.props.onRouteChange('home')
+			}
+		})
+	}
+
+    render() {
+        return (
+            <div>
+                <h1 className="header-text signup-header">Sign Up</h1>
+                <form onSubmit={this.onSubmitSignUp}>
+                    <input name="name" id="name" type="text" onChange={this.onFullNameChange} />
+                    <input type="submit"/>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default SignUp;
