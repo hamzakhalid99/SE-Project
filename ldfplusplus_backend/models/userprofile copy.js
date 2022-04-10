fe9c289723757a00e006badef3cb2d20a7f2aa58
Sheharyar-Khalid <87021517+Sheharyar-Khalid@users.noreply.github.com>
2022-04-10 17:01:51 +0500

    empty commit

---
+++ ldfplusplus_backend/models/userprofile copy.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const usertemplate = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    image:{
+        type:Buffer
+    },
+    adminstatus:{
+        type:Boolean,
+        default:false
+    },
+    status:{
+        type:[String]
+    },
+    superadmin:{
+        type:Boolean,
+        default:false
+    }
+})
+
+module.exports = mongoose.model('userprofile', usertemplate)
