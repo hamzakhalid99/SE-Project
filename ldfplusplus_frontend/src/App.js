dacbad5c16f02681279f073471a9a93c1cfab019
hamzakhalid99 <hamza.k.malik@outlook.com>
2022-04-17 03:03:46 +0500

    final

--- ldfplusplus_frontend/src/App.js
+++ ldfplusplus_frontend/src/App.js
@@ -10,6 +10,8 @@ import Homepage from './components/Homepage/Homepage.js';
 import FoodDelivery from './components/FoodDelivery/FoodDelivery.js';
 import FoodDeliveryPostDetails from './components/FoodDelivery/FoodDeliveryPostDetails.js';
 import FoodDeliveryNewPost from './components/FoodDelivery/FoodDeliveryNewPost.js';
+import FoodDeliveryMyPosts from './components/FoodDelivery/FoodDeliveryMyPosts.js';
+
 
 const initialState = {
 	route: 'landing',
@@ -236,7 +238,11 @@ class App extends Component {
       return (
         <div className="App">
           <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
-          <h1>view my food delivery posts</h1>
+          <FoodDeliveryMyPosts
+          onRouteChange={this.onRouteChange}
+          loadUser={this.loadUser}
+          user_id={this.state.user.id}
+          />
         </div>
       )
     }
@@ -248,7 +254,9 @@ class App extends Component {
           <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
           <FoodDeliveryNewPost
           onRouteChange={this.onRouteChange}
-          loadUser={this.loadUser}/>
+          loadUser={this.loadUser}
+          user_id={this.state.user.id}
+          />
         </div>
       )
     }
